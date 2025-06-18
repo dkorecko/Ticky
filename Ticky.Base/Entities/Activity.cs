827e2013b84772dd26f16336322fc179884a2099
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-06-18 11:09:09 +0200

    done

--- Ticky.Base/Entities/Activity.cs
+++ Ticky.Base/Entities/Activity.cs
@@ -5,7 +5,6 @@ public class Activity : AbstractDbEntity
     public required int UserId { get; set; }
     public virtual User User { get; set; } = null!;
     public required string Text { get; set; }
-    public required DateTime At { get; set; }
     public required int CardId { get; set; }
     public virtual Card Card { get; set; } = null!;
 }
