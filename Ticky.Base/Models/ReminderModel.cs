607074a60d6a79376485bf2030ebe884579dfc1c
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-06-18 17:16:23 +0200

    fix: Color scheme incorrectly set (#28)

--- Ticky.Base/Models/ReminderModel.cs
+++ Ticky.Base/Models/ReminderModel.cs
@@ -1,4 +1,4 @@
-﻿namespace Ticky.Base.Models;
+namespace Ticky.Base.Models;
 
 public class ReminderModel
 {
@@ -8,7 +8,7 @@ public class ReminderModel
 
     [Required(AllowEmptyStrings = false)]
     [RegularExpression(
-        "^[0-2][0-9]:[0-5][0-9]$",
+        "^([01][0-9]|2[0-3]):[0-5][0-9]$",
         ErrorMessage = "The time must be in HH:mm format."
     )]
     public string Time { get; set; } = string.Empty;
