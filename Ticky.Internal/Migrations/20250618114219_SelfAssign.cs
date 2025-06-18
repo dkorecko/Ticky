7cf9c6f3f7e2b401666ab12437942128c70122a7
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-06-18 14:00:57 +0200

    done

---
+++ Ticky.Internal/Migrations/20250618114219_SelfAssign.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticky.Internal.Migrations
+{
+    /// <inheritdoc />
+    public partial class SelfAssign : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AutomaticAssign",
+                table: "AspNetUsers",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutomaticAssign",
+                table: "AspNetUsers");
+        }
+    }
+}
