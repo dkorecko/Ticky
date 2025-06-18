f75804767790662dd70581f6bf4e0805ba74756a
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-06-18 17:28:51 +0200

    refactor: Remove At from Activities as not needed (#31)

---
+++ Ticky.Internal/Migrations/20250618090806_AtRemoval.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticky.Internal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtRemoval : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "At",
+                table: "Activities");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "At",
+                table: "Activities",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
