f25e19a66b4558050b0c5159632df2caf44c7571
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-07-03 16:11:37 +0200

    feat: Add board general settings tab and option to disable sort animations for performance (#54)

---
+++ Ticky.Internal/Migrations/20250703124731_DisableBoardAnimations.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticky.Internal.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisableBoardAnimations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DisableSortingAnimations",
+                table: "Boards",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisableSortingAnimations",
+                table: "Boards");
+        }
+    }
+}
