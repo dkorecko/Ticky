6ea80be0847934daee6f761624ed0653e582cb84
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-08-23 15:44:51 +0200

    added indication that the filter is applied

--- Ticky.Base/Models/FilterCardsModel.cs
+++ Ticky.Base/Models/FilterCardsModel.cs
@@ -3,4 +3,6 @@ namespace Ticky.Base.Models;
 public class FilterCardsModel
 {
     public string Text { get; set; } = string.Empty;
+
+    public bool IsAnyFilterApplied() => !string.IsNullOrWhiteSpace(Text);
 }
