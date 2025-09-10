c9b081ad72162b240e237a81aaabc99c6e169eb3
dkorecko <22599403+dkorecko@users.noreply.github.com>
2025-09-10 15:56:02 +0200

    works better

--- Ticky.Web/wwwroot/js/screenHeight.js
+++ Ticky.Web/wwwroot/js/screenHeight.js
@@ -1,19 +1,9 @@
-﻿function debounce(func, delay = 10) {
-    let timerId;
-    return function (...args) {
-        clearTimeout(timerId);
-        timerId = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
-}
-
-function setViewportHeight() {
+﻿function setViewportHeight() {
     document.documentElement.style.setProperty('--vvh', `${window.visualViewport.height * 0.01}px`);
 }
 
 // Set the height on initial load and when the keyboard appears/disappears
-window.visualViewport.addEventListener('resize', debounce(setViewportHeight));
+window.visualViewport.addEventListener('resize', setViewportHeight);
 
 // Call it once on page load to set the initial value
 setViewportHeight();
